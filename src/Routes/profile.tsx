872e6066fd2e5df24855d28ba890f95e167f8df3
chinmay_gujar07 <chinmaygujar52@gmail.com>
2023-10-17 22:11:02 +0530

    user request interface

--- src/Routes/profile.tsx
+++ src/Routes/profile.tsx
@@ -1,10 +1,16 @@
 // import { profile } from 'console';
 import { authenticateToken } from '../middleware/authMiddleware';
 import express from 'express';
+import { Request, Response } from 'express';
+
+// Define a custom interface for the request object
+interface AuthenticatedRequest extends Request {
+    user: any; // Replace 'any' with the actual type of your user data
+}
 const profileRouter = express.Router();
-profileRouter.get('/', authenticateToken, (req, res) => {
+profileRouter.get('/', authenticateToken, (req:Request, res:Response) => {
     // User is authenticated, you can access req.user for user data
-    const user = req.user;
+    const user = (req as AuthenticatedRequest).user;
     res.json({ user });
 });
 
